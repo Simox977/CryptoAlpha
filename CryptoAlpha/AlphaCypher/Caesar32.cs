4a788793bde3cb56ddb0da39496042084163c905
Simone Bruschini <flippatodimerda@gmail.com>
2015-10-27 11:28:57 +0100

    Secondo Commit lavoro codice Caesar32

--- CryptoAlpha/AlphaCypher/Caesar32.cs
+++ CryptoAlpha/AlphaCypher/Caesar32.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AlphaCypher
 {
-    class Caesar32
+   public  class Caesar32
     {
+        private List<Char> _vettAlfabeto;
+        private int _spiazzamento;
+
+        public Caesar32():base()
+        {
+
+        }
+        public Caesar32 (int spiazzamento) : base()
+        {
+            _spiazzamento = spiazzamento;
+        }
+
+        public string Codifica(string testo, int spiazzamento)
+        {
+            string resp = "";
+
+            resp = base.Codifica(testo, spiazzamento);
+            return resp;
+        }
     }
 }
